8cc8441f4495195bd5c4d329365d78429e6d7a86
venky <venkata-ravamma.vanipenta@capgemini.com>
2023-12-15 00:08:27 +0530

    view templateurl of header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  //template: '<h2>Ecart</h2>',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
