80e9595cfed28b5eb9af9cba3ec4dad9fe824ba6
venky <venkata-ravamma.vanipenta@capgemini.com>
2023-12-15 19:26:04 +0530

    Style url example

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,10 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   //template: '<h2>Ecart</h2>',
   templateUrl: './header.component.html',
-  styles: [
-    'a{text-decoration:none;margin:0 10px}',
-    'button{padding:10px 20px;}',
-    '.ekart--header{width:100px;height:70px}',
-  ],
+  // styles: ['a{text-decoration:none;margin:0 10px}',  'button{padding:10px 20px;}','.ekart--header{width:100px;height:70px}', ],
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
