3608c4ec4f88bdd5e956ddc83f57c341f62e5f5f
venky <venkata-ravamma.vanipenta@capgemini.com>
2023-12-15 18:55:39 +0530

    Style view property

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,5 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   //template: '<h2>Ecart</h2>',
   templateUrl: './header.component.html',
+  styles: [
+    'a{text-decoration:none;margin:0 10px}',
+    'button{padding:10px 20px;}',
+    '.ekart--header{width:100px;height:70px}',
+  ],
 })
 export class HeaderComponent {}
