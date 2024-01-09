a0329d48289b55c1d06463043ac1fb2781525d9f
venky <v.veky9815@gmail.com>
2024-01-09 20:17:14 +0530

    @Input() custom property binding decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ContainerComponent } from './container/container.component';
 import { SearchComponent } from './container/search/search.component';
 import { FormsModule } from '@angular/forms';
 import { ProductListComponent } from './container/product-list/product-list.component';
+import { ProductComponent } from './container/product-list/product/product.component';
+import { FilterComponent } from './container/product-list/filter/filter.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { ProductListComponent } from './container/product-list/product-list.comp
     ContainerComponent,
     SearchComponent,
     ProductListComponent,
+    ProductComponent,
+    FilterComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
---
+++ src/app/container/product-list/filter/filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css'],
+})
+export class FilterComponent {
+  @Input()
+  all: number = 0;
+  @Input()
+  inStock: number = 0;
+  @Input()
+  outofStock: number = 0;
+}
