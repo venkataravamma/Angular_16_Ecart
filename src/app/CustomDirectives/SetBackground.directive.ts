794f77845fdb50a6521e63fcf1e35fe436cd5489
venky <v.veky9815@gmail.com>
2024-01-31 15:51:13 +0530

    #48 Renderer2 in Angular

--- src/app/CustomDirectives/SetBackground.directive.ts
+++ src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,16 +1,29 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
 })
 export class SetBackground implements OnInit {
   // private element: ElementRef;
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, private render: Renderer2) {
     // this.element = element;
   }
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = '#36454f';
-    this.element.nativeElement.style.color = 'white';
+    //elementRef get the reference of DOM directly not good practice
+    // this.element.nativeElement.style.backgroundColor = '#36454f';
+    // this.element.nativeElement.style.color = 'white';
+    //Render2 manipulate the Dom without access the DOM elements directly.
+    this.render.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      '#36454f'
+    );
+    this.render.setStyle(this.element.nativeElement, 'color', 'yellow');
+    this.render.setAttribute(
+      this.element.nativeElement,
+      'title',
+      'This is an example title'
+    );
   }
 }
